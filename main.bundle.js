a180a4eff6dd4a3e863f7953dc4672c8a733f9bc
PolyTrack Unblocked 0.6.0 <decency.kibbles.9k@icloud.com>
2026-04-01 16:47:37 +1000

    Update main.bundle.js

--- main.bundle.js
+++ main.bundle.js
@@ -70098,7 +70098,7 @@
         }
         getLeaderboard(e, t, n, i, r) {
           let a =
-            "https://polytrack-server-production.up.railway.app" +
+            "https://polytrack-server-production.up.railway.app/user" +
             "leaderboard?version=0.6.0&trackId=" +
             t +
             "&skip=" +
@@ -70299,7 +70299,7 @@
         }
         getLeaderboardUserEntry(e, t, n) {
           const i =
-            "https://polytrack-server-production.up.railway.app" +
+            "https://polytrack-server-production.up.railway.app/user" +
             "leaderboardUserEntry?version=0.6.0&trackId=" +
             t +
             "&userTokenHash=" +
@@ -70352,7 +70352,7 @@
         }
         getRecordings(e) {
           const t =
-            "https://polytrack-server-production.up.railway.app" +
+            "https://polytrack-server-production.up.railway.app/user" +
             "recordings?version=0.6.0&ids=" +
             e.join(",");
           return new Promise((e, n) => {
@@ -70465,7 +70465,7 @@
                 c(new Error("Recording is too large"));
               else {
                 const o =
-                  "https://polytrack-server-production.up.railway.app" +
+                  "https://polytrack-server-production.up.railway.app/user" +
                   "leaderboard";
                 let d =
                   "version=0.6.0&userToken=" +
@@ -70590,7 +70590,7 @@
         submitUserProfile(e, t, n, i) {
           return new Promise((r, a) => {
             const s =
-                "https://polytrack-server-production.up.railway.app" + "user",
+                "https://polytrack-server-production.up.railway.app/user" + "user",
               o =
                 "version=0.6.0&userToken=" +
                 encodeURIComponent(e) +
@@ -70622,7 +70622,7 @@
               s(new Error("Submit not allowed"));
             else {
               const o =
-                  "https://polytrack-server-production.up.railway.app" +
+                  "https://polytrack-server-production.up.railway.app/user" +
                   "verifyRecordings",
                 l =
                   "version=0.6.0&userToken=" +
@@ -70754,7 +70754,7 @@
         getUser(e) {
           return new Promise((t, n) => {
             const i =
-                "https://polytrack-server-production.up.railway.app" +
+                "https://polytrack-server-production.up.railway.app/user" +
                 "user?version=0.6.0&userToken=" +
                 encodeURIComponent(e),
               r = new XMLHttpRequest();
@@ -70821,7 +70821,7 @@
               "WebSocket creation not allowed with non-deterministic physics",
             );
           return new WebSocket(
-            "https://polytrack-server-production.up.railway.app" +
+            "https://polytrack-server-production.up.railway.app/user" +
               "multiplayer/host",
           );
         }
@@ -70831,14 +70831,14 @@
               "WebSocket creation not allowed with non-deterministic physics",
             );
           return new WebSocket(
-            "https://polytrack-server-production.up.railway.app" +
+            "https://polytrack-server-production.up.railway.app/user" +
               "multiplayer/join",
           );
         }
         getIceServers() {
           return new Promise((e, t) => {
             const n =
-                "https://polytrack-server-production.up.railway.app" +
+                "https://polytrack-server-production.up.railway.app/user" +
                 "iceServers?version=0.6.0",
               i = new XMLHttpRequest();
             ((i.timeout = (0, C.gn)(this, wu, "f")),
